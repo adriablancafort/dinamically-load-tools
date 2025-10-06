238a4612399f218e66c34b2c2645f4fbb6afdfef
Adrià <adriablancafort@gmail.com>
2025-10-06 03:04:36 +0200

    tool calling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { openai } from '@ai-sdk/openai';
-import { ModelMessage, streamText } from 'ai';
+import { ModelMessage, streamText, tool, stepCountIs } from 'ai';
 import 'dotenv/config';
+import { z } from 'zod';
 import * as readline from 'node:readline/promises';
 
 const terminal = readline.createInterface({
@@ -19,6 +20,43 @@ async function main() {
     const result = streamText({
       model: openai('gpt-4o'),
       messages,
+      tools: {
+        weather: tool({
+          description: 'Get the weather in a location (fahrenheit)',
+          inputSchema: z.object({
+            location: z
+              .string()
+              .describe('The location to get the weather for'),
+          }),
+          execute: async ({ location }) => {
+            const temperature = Math.round(Math.random() * (90 - 32) + 32);
+            return {
+              location,
+              temperature,
+            };
+          },
+        }),
+        convertFahrenheitToCelsius: tool({
+          description: 'Convert a temperature in fahrenheit to celsius',
+          inputSchema: z.object({
+            temperature: z
+              .number()
+              .describe('The temperature in fahrenheit to convert'),
+          }),
+          execute: async ({ temperature }) => {
+            const celsius = Math.round((temperature - 32) * (5 / 9));
+            return {
+              celsius,
+            };
+          },
+        }),
+      },
+      stopWhen: stepCountIs(5),
+      onStepFinish: async ({ toolResults }) => {
+        if (toolResults.length) {
+          console.log(JSON.stringify(toolResults, null, 2));
+        }
+      },
     });
 
     let fullResponse = '';
